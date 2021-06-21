0e0dcaec27fb0a61f781ff21e1cd8a1a5fe18235
Michael Mclaughlin <M8ch88l@gmail.com>
2021-06-21 18:31:41 +0100

    #146 Symbol.for to Symbol['for']

--- decimal.js
+++ decimal.js
@@ -4858,7 +4858,7 @@
   // Node and other environments that support module.exports.
   } else if (typeof module != 'undefined' && module.exports) {
     if (typeof Symbol == 'function' && typeof Symbol.iterator == 'symbol') {
-      P[Symbol.for('nodejs.util.inspect.custom')] = P.toString;
+      P[Symbol['for']('nodejs.util.inspect.custom')] = P.toString;
       P[Symbol.toStringTag] = 'Decimal';
     }
 
